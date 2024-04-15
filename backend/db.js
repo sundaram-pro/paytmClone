ed0e05c0dbfe06d7abad2838e4c639508723f301
Sundaram Sharma <sundaram-sharma@hotmail.com>
2024-04-16 00:10:07 +0530

    frontend backend created

---
+++ backend/db.js
@@ -0,0 +1,42 @@
+// backend/db.js
+const mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect("mongodb+srv://sundaramsharma:7q65sp1zGXxqyRGY@cluster0.asmird9.mongodb.net/")
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+// Create a model from the schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User
+};
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,18 @@
 const express = require("express");
+const cors = require("cors")
+const app = express();
+const mainRouter = require("./routes/index")
+const port = 3000
+
+app.use(cors())
+
+app.use("/api/v1", mainRouter) //this heads to main router for the v1 of the api
+
+app.use(express.json());
+
+app.listen(port,()=>{
+    console.log(`The app is running on the port${port}`)
+})
+
 
 
