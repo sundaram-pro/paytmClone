5bbc99fe43c920375f5789af7b312912d073529f
Sundaram Sharma <sundaram-sharma@hotmail.com>
2024-04-18 23:58:03 +0530

    signup route update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,11 +5,13 @@ const zod = require("zod");
 const router = express.Router();
 
 const signupSchema = zod.object({
-    username: zod.string(),
-    password: zod.string(),
+    username: zod.string().email(),
     firstname: zod.string(),
-    lastname: zod.string()
+    lastname: zod.string(),
+    password: zod.string()
 
 })
+
+
 
 module.exports = router;
