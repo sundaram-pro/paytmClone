8673c096eea83c4dfd2d7340574cb51ba707a5f0
Sundaram Sharma <sundaram-sharma@hotmail.com>
2024-04-18 23:54:45 +0530

    singup route added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,4 +4,12 @@ const zod = require("zod");
 
 const router = express.Router();
 
+const signupSchema = zod.object({
+    username: zod.string(),
+    password: zod.string(),
+    firstname: zod.string(),
+    lastname: zod.string()
+
+})
+
 module.exports = router;
